8db22ff61763841ab46a5c9673931c894e08bbf9
ravisstrc <raviss.trc@gmail.com>
2024-11-17 12:41:19 +0530

    updated configurations

--- src/main/java/com/org/ravi/community/SocialCommunityApplication.java
+++ src/main/java/com/org/ravi/community/SocialCommunityApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class SocialCommunityApplication {
 
 	public static void main(String[] args) {
